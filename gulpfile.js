7dd3a9174fe25c8fd17b6953f5209a0456a8917d
Pete Martin <petemartinuk@gmail.com>
2017-10-13 14:05:45 +0000

    build cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var browserify, concat, connect, filter, gulp;
+var browserify, concat, connect, filter, gulp, plumber, logger, clean;
 
 gulp = require('gulp');
+clean = require('gulp-clean');
 plumber = require('gulp-plumber');
 filter = require('gulp-filter');
 logger = require('gulp-logger');
 concat = require('gulp-concat');
 connect = require('gulp-connect');
 browserify = require('gulp-browserify');
+
+gulp.task('clean', function() {
+  gulp
+    .src(['./game.js', './phaser.js'], {read: false})
+    .pipe(clean());
+});
 
 gulp.task('build', function() {
   gulp
@@ -39,10 +46,10 @@ gulp.task('connect', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./game/**/*.js', ['build']);
+  gulp.watch('./game/**/*.js', ['clean', 'vendor', 'build']);
 });
 
 gulp.task('default', function() {
-  gulp.run('vendor', 'build', 'connect', 'watch');
+  gulp.run('clean', 'vendor', 'build', 'connect', 'watch');
 });
 
