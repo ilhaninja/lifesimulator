74bb07059d56c938980bba4d17a4425533e269c2
Pete Martin <petemartinuk@gmail.com>
2017-10-13 14:21:12 +0000

    preload sprite atlases

--- game/states/preload.js
+++ game/states/preload.js
@@ -9,9 +9,25 @@ Preload.prototype = {
   preload: function() {
     this.asset = this.add.sprite(this.width / 2, this.height / 2, 'preloader');
     this.asset.anchor.setTo(0.5, 0.5);
+
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
-    return this.load.image('yeoman', 'assets/yeoman-logo.png');
+
+    this.load.image('yeoman', 'assets/yeoman-logo.png');
+
+    var characterAssets = ['face', 'hair', 'pants', 'shirts', 'shoes', 'skin'];
+    var tileAssets = ['building-tiles', 'city-details', 'city-tiles', 'landscape-tiles', 'vehicles'];
+    var uiAssets = ['blue', 'green', 'grey', 'items', 'red', 'tab-controls', 'yellow'];
+
+    for (let asset of characterAssets) {
+      this.load.atlasXML('character-'+asset, 'assets/character/'+asset+'.png', 'assets/character/'+asset+'.xml');
+    }
+    for (let asset of tileAssets) {
+      this.load.atlasXML(asset, 'assets/tiles/'+asset+'.png', 'assets/tiles/'+asset+'.xml');
+    }
+    for (let asset of uiAssets) {
+      this.load.atlasXML('ui-'+asset, 'assets/ui/'+asset+'.png', 'assets/ui/'+asset+'.xml');
+    }
   },
   create: function() {
     return this.asset.cropEnabled = false;
