f17b9b7a4d07db56da7087fcf548f25dfe1daf95
Pete Martin <petemartinuk@gmail.com>
2017-10-13 19:40:55 +0000

    nineslice scaling and gfx

---
+++ game/prefabs/nineslice.js
@@ -0,0 +1,56 @@
+var NineSlice = function(game, x, y, color, key, width, height, gutter) {
+  Phaser.Group.call(this, game);
+
+  console.log('made it this far');
+
+  var atlas = 'ui-'+color;
+  var frame = color+'-'+key;
+  var ext = '.png';
+
+  var suffixes = [
+    ['ul', 'u', 'ur'],
+    ['l', 'c', 'r'],
+    ['bl', 'b', 'br']
+  ];
+
+  var anchors = [
+    [[ 0 , 0 ], [0.5,  0 ], [ 1 ,  0 ]],
+    [[ 0 ,0.5], [0.5, 0.5], [ 1 , 0.5]],
+    [[ 0 , 1 ], [0.5,  1 ], [ 1 ,  1 ]]
+  ];
+
+  var w = x+width/2, h = y+height/2, wn = x-width/2, hn = y-height/2;
+  var centers = [
+    [[wn,hn], [ x,hn], [ w,hn]],
+    [[wn, y], [ x, y], [ w, y]],
+    [[wn, h], [ x, h], [ w, h]]
+  ];
+
+
+  var g = gutter;
+  var w = width - 2 * g;
+  var h = height -2 * g;
+  var sizes = [
+    [[g,g],[w,g],[g,g]],
+    [[g,h],[w,h],[g,h]],
+    [[g,g],[w,g],[g,g]]
+  ];
+
+  var sprites = [[],[],[]];
+  for (var i=0; i<3; i++) {
+    console.log(i);
+    for (var j=0; j<3; j++) {
+      sprites[i][j] = this.create(centers[i][j][0], centers[i][j][1], atlas, frame+'-'+suffixes[i][j]+ext);
+      sprites[i][j].anchor.setTo(anchors[i][j][0], anchors[i][j][1]);
+      sprites[i][j].width = sizes[i][j][0];
+      sprites[i][j].height= sizes[i][j][1];
+    }
+  }
+
+};
+
+NineSlice.prototype = Object.create(Phaser.Group.prototype);
+NineSlice.prototype.constructor = NineSlice;
+
+module.exports = NineSlice;
+
--- game/states/menu.js
+++ game/states/menu.js
@@ -7,20 +7,24 @@ Menu.prototype = {
   preload: function() {},
 
   create: function() {
-    var title, instructions, game = this.game, world = this.game.world, fonts = this.game.fonts;
+    var title, subtitle, game = this.game, world = this.game.world, fonts = this.game.fonts;
 
-    var title = game.add.text(world.centerX, 380, 'LIFE', fonts.large);
-    var instructions = game.add.text(world.centerX, 420, 'TAP', fonts.medium);
+    var titleFont = Object.create(fonts.huge);
+    titleFont.fill = game.colors.blue;
 
+    var subtitleFont = Object.create(fonts.medium);
+    subtitleFont.fill = game.colors.blue;
+
+    window.greyPanel = new game.prefabs.NineSlice(game, world.centerX, world.centerY, 'grey', 'panel', 400, 240, 6);
+
+    title = game.add.text(world.centerX, 400, 'LIFE', titleFont);
+    subtitle = game.add.text(world.centerX, 460, 'A SIMULATOR', subtitleFont);
+    
     title.anchor.setTo(0.5, 0.5);
-    instructions.anchor.setTo(0.5, 0.5);
+    subtitle.anchor.setTo(0.5, 0.5);
   },
-  update: function() {
-    var game = this.game;
 
-    if (game.input.activePointer.justPressed()) {
-      game.state.start('play');
-    }
+  update: function() {
   }
 };
 
